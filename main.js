f1131a42f69b7c9746aa6e9e70a8e4bbf62af599
kaycee04 <kingsleyotuya04@gmail.com>
2024-02-27 13:46:58 +0100

    account infos

---
+++ main.js
@@ -0,0 +1,33 @@
+function myTextOne() {
+  const copyText = document.getElementById("numb1");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  alert("copied");
+}
+
+function myTextTwo() {
+  const copyText = document.getElementById("numb2");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+//   alert("copied" + copyText.value);
+}
+function myTextThree() {
+  const copyText = document.getElementById("numb3");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+//   alert("copied" + copyText.value);
+}
+function myTextFour() {
+  const copyText = document.getElementById("numb4");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+//   alert("copied" + copyText.value);
+}
